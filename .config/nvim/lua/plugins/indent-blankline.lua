fd0e3f2a516bd4f49b600180de2b242081d2ab67
Jose Vega <jvegar@uni.pe>
2025-07-09 01:44:09 -0500

    feat(plugins): configure indent-blankline guides
    Customize indent-blankline.nvim plugin with:
    - Set vertical line character for indent guides
    - Add custom highlight group for subtle gray color (#444444)
    - Configure hooks for proper highlight setup

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,7 +1,19 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  ---@module "ibl"
-  ---@type ibl.config
-  opts = {},
+  opts = {
+    indent = {
+      char = "│",
+      highlight = { "IndentBlanklineCharLight" },
+    },
+  },
+  config = function(_, opts)
+    local hooks = require("ibl.hooks")
+    -- Create the highlight group using the hook
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "IndentBlanklineCharLight", { fg = "#444444" })
+    end)
+    -- Call the plugin's setup function with the options
+    require("ibl").setup(opts)
+  end,
 }
