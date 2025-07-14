2c9b68f62c6a774c0c5d687ef0782899672ba749
Jose Vega <jvegar@uni.pe>
2025-07-14 12:54:09 -0500

    feat(plugins): add telescope dependency to alpha-nvim
    Adds telescope.nvim as a dependency to alpha-nvim plugin to ensure proper
    loading order and functionality integration.

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,7 +1,7 @@
 return {
   "goolord/alpha-nvim",
   enabled = true,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { "nvim-tree/nvim-web-devicons", "nvim-telescope/telescope.nvim" },
   config = function()
     local alpha = require("alpha")
     local startify = require("alpha.themes.startify")
