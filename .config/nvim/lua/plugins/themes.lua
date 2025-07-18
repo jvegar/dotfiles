94b4e504d6c7ef70d1ab1fc7e73a4fbd2df30a12
Jose Vega <jvegar@uni.pe>
2025-07-18 16:10:17 -0500

    refactor(themes): simplify theme configuration
    * Remove unused theme plugins (citruszest, newpaper, solarized-osaka)
    * Keep only leaf.nvim as the primary theme
    * Fix formatting and indentation in vim-options.lua

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,44 +1,18 @@
 return {
-  {
-    "daschw/leaf.nvim",
-    config = function()
-      require("leaf").setup({
-        theme = "dark",
-        transparent = true,
-        italics = {
-          comments = false,
-          keywords = false,
-          functions = false,
-          strings = false,
-          variables = false,
-        },
-      })
-    end,
-  },
-  {
-    "zootedb0t/citruszest.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "yorik1984/newpaper.nvim",
-    priority = 1000,
-    config = function()
-      require("newpaper").setup({
-        style = "light",
-        lightness = -0.1,
-        saturation = -0.1,
-      })
-    end,
-  },
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = true,
-    priority = 1000,
-    opts = function()
-      return {
-        transparent = true
-      }
-    end
-  }
+	{
+		"daschw/leaf.nvim",
+		config = function()
+			require("leaf").setup({
+				theme = "dark",
+				transparent = true,
+				italics = {
+					comments = false,
+					keywords = false,
+					functions = false,
+					strings = false,
+					variables = false,
+				},
+			})
+		end,
+	},
 }
