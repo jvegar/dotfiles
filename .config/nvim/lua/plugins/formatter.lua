df98ed5b3501d463479444f074d53d177953f37b
Jose Vega <jvegar@uni.pe>
2025-10-21 22:24:45 -0500

    feat(plugins): add nvim-autopairs for auto-pairing
    Add windwp/nvim-autopairs plugin to automatically insert and handle
    matching pairs of brackets, parentheses, and quotes in insert mode.
    Configure with default settings.

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -36,4 +36,11 @@ return {
 			},
 		},
 	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
 }
